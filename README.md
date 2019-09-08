644c86cafd816e043e5ebd0e52b0f10b0fc94ef8
Rick Strahl <rstrahl@west-wind.com>
2019-09-07 22:48:12 -0700

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,175 @@
+# HighlightJS Copy Code and Syntax Badge
+
+This small JavaScript library that complements the [highlightJs Syntax Highligher](https://highlightjs.org/) by providing a badge in top right corner that:
+
+* Shows active Syntax for the code block
+* Allows copying the code block to Clipboard
+
+Here's what this looks like:
+
+![](ScreenShot.png)
+
+This small JavaScript file contains a single file that can be loaded after highlightJS has been loaded and is fully self-contained - add the script and it works without additional dependencies. 
+
+### Usage
+To use this library is very simple - you add a script file and call `highlightJsBadge()` after highlightJS has been applied.
+
+```html
+<link href="scripts/highlightjs/styles/vs2015.css" rel="stylesheet" />
+<script src="scripts/highlightjs/highlight.pack.js"></script>
+<script src="scripts/highlightjs-badge.min.js"></script>
+<script>
+    // apply HighlightJS
+    var pres = document.querySelectorAll("pre>code");
+    for (var i = 0; i < pres.length; i++) {
+       hljs.highlightBlock(pres[i]);
+    }
+    
+    // add HighlightJS-badge
+    var options = {   // optional
+       contentSelector: "#ArticleBody",
+       
+       // CSS class(es) used to render the copy icon.
+       copyIconClass: "fas fa-copy",
+       // CSS class(es) used to render the done icon.
+       checkIconClass: "fas fa-check text-success"
+    };
+    window.highlightJsBadge(options);
+</script>
+```
+
+### Styling
+The default script includes default styling that should work great with dark themed syntax, and fairly well with light themed syntax.
+
+You can customize the styling and the layout of badge by either overriding existing styles or by:
+
+* Overriding styles
+* Copying complete styles and template into page
+
+#### Overriding styles
+The easiest way to modify behavior is to override individual styles. The stock script includes a hardcoded style sheet and you can override the existing values with hard CSS overrides.
+
+For example to override the background and icon sizing you can:
+
+```css
+<style>
+    .code-badge {
+        padding: 8px !important;
+        background: pink !important;
+    }
+    .code-badge-copy-icon {
+        font-size: 1.3em !important;
+    }
+</style>
+```
+
+#### Replace the Template and Styling Completely
+Alternately you can completely replace the template and styling. If you look at the source file at the end of the file is a commented section that contains the complete template and you can copy and paste that template into your HTML page - at the bottom near the `</body>` tag.
+
+```html
+<style>
+    pre>code.hljs {
+        position: relative;
+    }
+    .fa.text-success:{ color: limegreen !important}
+
+    .code-badge {
+        display: flex;
+        flex-direction: row;
+        white-space: normal;
+        background: transparent;
+        background: #333;
+        color: white;
+        font-size: 0.8em;
+        opacity: 0.5;
+        border-radius: 0 0 0 7px;
+        padding: 5px 8px 5px 8px;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .code-badge.semi-active {
+        opacity: .50
+    }
+
+    .code-badge.active {
+        opacity: 0.8;
+    }
+
+    .code-badge:hover {
+        opacity: .95;
+    }
+
+    .code-badge a,
+    .code-badge a:hover {
+        text-decoration: none;
+    }
+
+    .code-badge-language {
+        margin-right: 10px;
+        font-weight: 600;
+        color: goldenrod;
+    }
+    .code-badge-copy-icon {
+        font-size: 1.2em;
+        cursor: pointer;
+        padding: 0 7px;
+        margin-top:2;
+    }
+</style>
+<div id="CodeBadgeTemplate" style="display:none">
+    <div class="code-badge">
+        <div class="code-badge-language">{{language}}</div>
+        <div title="Copy to clipboard">
+            <i class="{{copyIconClass}} code-badge-copy-icon"></i>
+        </div>
+     </div>
+</div>
+```
+
+This is the same template that the library internally holds and injects into the page, but if the `CodeBadgeTemplate` exists in the document then that is used instead of the embedded template. At this point you can override the styling and layout as needed.
+
+### Requirements
+This library is a self-contained JavaScript file so there are no direct external dependencies. However there are a couple of requirements:
+
+#### Some ES6 Usage
+The library uses a couple of ES6 functions:
+
+* [Object.assign](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill)
+* [String.trim](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim#Polyfill)
+
+and you can add the polyfills linked above for old browser (Internet Explorer) support.
+
+#### FontAwesome by Default, Custom Icon Overrides
+The library by default also uses [FontAwesome icons](https://fontawesome.com/icons?from=io) for the copy and check icons. The icon styles used work with Font Awesome 4 and 5 (both free and pro). 
+
+The default rendering uses:
+
+```html
+<i class="fa fa-copy"></i>
+```
+The icon is changed to `fa-check` check after content's been copied for 2 seconds.
+
+If you want to use different icons you can use separate styling for the icon classes. For example to change the icons used you affect the `class` attribute on the icon setting. 
+
+For example to use Material Icons:
+
+```javascript
+var options = {
+        // CSS class(es) used to render the copy icon.
+        copyIconClass: "material-icons",  
+        copyIconContent: "copy_files",
+        
+        // CSS class(es) used to render the done icon.
+        checkIconClass: "material-icons",
+        checkIconContent: "check"
+};
+window.highlightJsBadge(options);
+```
+
+### License 
+Licensed under the MIT License. There's no charge to use, integrate or modify the code for this project. You are free to use it in personal, commercial, government and any other type of application.
+
+
+
---
+++ highlightjs-badge.js
@@ -0,0 +1,390 @@
+"use strict";
+/*
+----------------------------------------
+highlightJs Badge
+----------------------------------------
+
+A copy code and language display badge
+for the highlightJs Syntax highlighter.
+
+by Rick Strahl, 2019
+License: MIT
+
+Make sure this script is loaded last in your
+script loading.
+
+Usage:
+------
+Load `highlightjs-badge` after `highlight.js`:
+
+```js
+<link href="vs2015.css" rel="stylesheet">
+<script src="highlight.pack.js"></script>
+
+<script src="highlightjs-badge.js" async></script>  
+```
+
+The script contains the template and CSS so nothing
+else is needed to execute it.
+
+Customization:
+--------------
+This code automatically embeds styling and the template.
+
+If you want to customize you can either create a template
+in your HTML using the code at the end of this file.
+
+Alternately you can customize the `getTemplate()` function
+that renders the code from a string and keep it self contained
+within this script.
+
+Requirements:
+-------------
+Uses some ES6 features so won't work in IE without shims:
+
+* Object.assign
+* String.trim
+
+*/
+
+// module header
+(function( global, factory ) {
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		// For CommonJS and CommonJS-like environments where a proper `window`
+		// is present, execute the factory		
+		module.exports = global.document ?
+			factory( global, true ) :
+			function( w ) {
+				if ( !w.document ) {
+					throw new Error( "A window with a document is required" );
+				}
+				return factory( w );
+			};
+	} else {
+		factory( global );
+	}
+
+// Pass this if window is not defined yet
+}(typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+
+
+    
+if (typeof highlightJsBadgeAutoLoad !== 'boolean')
+    var highlightJsBadgeAutoLoad = false;
+
+function highlightJsBadge(opt) {
+    var options = {
+        // the selector for the badge template
+        templateSelector: "#CodeBadgeTemplate",
+
+        // base content selector that is searched for snippets
+        contentSelector: "body",
+
+        // Delay in ms used for `setTimeout` before badging is applied
+        // Use if you need to time highlighting and badge application
+        // since the badges need to be applied afterwards.
+        // 0 - direct execution (ie. you handle timing
+        loadDelay: 0,
+
+        // CSS class(es) used to render the copy icon.
+        copyIconClass: "fa fa-copy",     
+        // optional content for icons class (<i class="fa fa-copy"></i> or <i class="material-icons">file_copy</i>)
+        copyIconContent: "",
+
+        // CSS class(es) used to render the done icon.
+        checkIconClass: "fa fa-check text-success",
+        checkIconContent: ""  
+    };
+
+    function initialize(opt) {
+        Object.assign(options, opt);
+
+        if (document.readyState == 'loading')
+            document.addEventListener("DOMContentLoaded", load);
+        else
+            load();
+    }
+
+
+    function load() {
+        if (options.loadDelay)
+            setTimeout(addCodeBadge, loadDelay);
+        else
+            addCodeBadge();
+    }
+
+    function addCodeBadge() {
+        // first make sure the template exists - if not we embed it
+        if (!document.querySelector(options.templateSelector)) {
+            var node = document.createElement("div");
+            node.innerHTML = getTemplate();            
+            var style = node.querySelector("style");
+            var template = node.querySelector(options.templateSelector);
+            document.body.appendChild(style);
+            document.body.appendChild(template);
+        }
+
+        var $codes = document.querySelectorAll("pre>code.hljs");
+
+        var hudText = document.querySelector(options.templateSelector).innerHTML;
+
+        for (var index = 0; index < $codes.length; index++) {
+            var el = $codes[index];
+            if (el.querySelector(".code-badge"))
+                continue; // already exists
+
+            var lang = "";
+
+            for (var i = 0; i < el.classList.length; i++) {
+                if (el.classList[i].substr(0, 9) === 'language-') {
+                    lang = el.classList[i].replace('language-', '');
+                    break;
+                }
+            }
+
+            if (lang)
+                lang = lang.toLowerCase();
+            else
+                lang = "text";
+
+            if (lang == "ps")
+                lang = "powershell";
+            else if (lang == "cs")
+                lang = "csharp";
+            else if (lang == "js")
+                lang = "javascript";
+            else if (lang == "ts")
+                lang = "typescript";
+            else if (lang == "fox")
+                lang = "foxpro";
+
+                
+            var html = hudText.replace("{{language}}", lang)
+                              .replace("{{copyIconClass}}",options.copyIconClass)
+                              .trim();
+
+            // insert the Hud panel
+            var $newHud = document.createElement("div");
+            $newHud.innerHTML = html;
+            $newHud = $newHud.querySelector(".code-badge");
+            $newHud.style.display = "flex";
+
+            if(options.copyIconContent)
+              $newHud.querySelector(".code-badge-copy-icon").innerText = options.copyIconContent;
+
+            el.insertBefore($newHud, el.firstChild);
+        }
+
+        var $content = document.querySelector(options.contentSelector);
+
+        $content.addEventListener("click",
+            function (e) {                               
+                var $clicked = e.srcElement;
+                if ($clicked.classList.contains("code-badge-copy-icon")) {
+                    e.preventDefault();
+                    e.cancelBubble = true;
+                    copyCodeToClipboard(e);
+                }
+                return false;
+            });
+    }
+
+    function copyCodeToClipboard(e) {
+           
+        var $origCode = e.srcElement.parentElement.parentElement.parentElement;
+
+        // we have to clear out the .code-badge - clone and remove
+        var $code = $origCode.cloneNode(true);
+        var $elHud = $code.querySelector(".code-badge");
+        $elHud.innerHTML = ""; // create text
+
+        var text = $code.innerText;
+        var el = document.createElement('textarea');
+
+        el.value = text.trim();
+        document.body.appendChild(el);
+        el.style.display = "block";
+
+        if (window.document.documentMode)
+            el.setSelectionRange(0, el.value.length);
+        else
+            el.select();
+        
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        
+        swapIcons($origCode);     
+    }
+
+    function swapIcons($code) {
+        var copyIcons = options.copyIconClass.split(' ');
+        var checkIcons = options.checkIconClass.split(' ');
+        
+        var $fa = $code.querySelector(".code-badge-copy-icon");
+        $fa.innerText = options.checkIconContent;
+
+        for (var i = 0; i < copyIcons.length; i++)
+            $fa.classList.remove(copyIcons[i]);
+        
+        for (var i = 0; i < checkIcons.length; i++)
+            $fa.classList.add(checkIcons[i]);
+        
+        
+        setTimeout(function () {
+            $fa.innerText = options.copyIconContent;
+
+            for (var i = 0; i < checkIcons.length; i++)
+                $fa.classList.remove(checkIcons[i]);
+            for (var i = 0; i < copyIcons.length; i++)
+                $fa.classList.add(copyIcons[i]);
+        }, 2000);
+    }
+
+    function getTemplate() {
+        var stringArray =
+        [
+            "<style>",
+            "    pre>code.hljs {",
+            "        position: relative;",
+            "    }",
+            "    .fa.text-success:{ color: limegreen !important}",
+            "    .code-badge {",
+            "        display: flex;",
+            "        flex-direction: row;",
+            "        white-space: normal;",
+            "        background: transparent;",
+            "        background: #333;",
+            "        color: white;",
+            "        font-size: 0.8em;",
+            "        opacity: 0.5;",
+            "        transition: opacity linear 0.5s;",
+            "        border-radius: 0 0 0 7px;",
+            "        padding: 5px 8px 5px 8px;",
+            "        position: absolute;",
+            "        right: 0;",
+            "        top: 0;",
+            "    }",
+            "",
+            "    .code-badge.semi-active {",
+            "        opacity: .50",
+            "    }",
+            "",
+            "    .code-badge.active {",
+            "        opacity: 0.8;",
+            "    }",
+            "",
+            "    .code-badge:hover {",
+            "        opacity: .95;",
+            "    }",
+            "",
+            "    .code-badge a,",
+            "    .code-badge a:hover {",
+            "        text-decoration: none;",
+            "    }",
+            "",
+            "    .code-badge-language {",
+            "        margin-right: 10px;",
+            "        font-weight: 600;",
+            "        color: goldenrod;",
+            "    }",
+            "    .code-badge-copy-icon {",
+            "        font-size: 1.2em;",
+            "        cursor: pointer;",
+            "        padding: 0 7px;",
+            "        margin-top:2;",
+            "    }",
+            "</style>",
+            "<div id=\"CodeBadgeTemplate\" style=\"display:none\">",
+            "    <div class=\"code-badge\">",
+            "        <div class=\"code-badge-language\" >{{language}}</div>",
+            "        <div  title=\"Copy to clipboard\">",
+            "            <i class=\"{{copyIconClass}} code-badge-copy-icon\"></i></i></a>",            
+            "        </div>",
+            "     </div>",
+            "</div>"
+        ];
+
+        var t = "";
+        for (var i = 0; i < stringArray.length; i++)
+            t += stringArray[i] + "\n";
+
+        return t;
+    }
+
+    initialize();
+}
+
+
+// global reference Window
+window.highlightJsBadge = highlightJsBadge;
+
+if (highlightJsBadgeAutoLoad)
+    highlightJsBadge();
+
+
+}));
+
+
+
+/*
+<style>
+    pre>code.hljs {
+        position: relative;
+    }
+    .fa.text-success:{ color: limegreen !important}
+
+    .code-badge {
+        display: flex;
+        flex-direction: row;
+        white-space: normal;
+        background: transparent;
+        background: #333;
+        color: white;
+        font-size: 0.8em;
+        opacity: 0.5;
+        border-radius: 0 0 0 7px;
+        padding: 5px 8px 5px 8px;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .code-badge.semi-active {
+        opacity: .50
+    }
+
+    .code-badge.active {
+        opacity: 0.8;
+    }
+
+    .code-badge:hover {
+        opacity: .95;
+    }
+
+    .code-badge a,
+    .code-badge a:hover {
+        text-decoration: none;
+    }
+
+    .code-badge-language {
+        margin-right: 10px;
+        font-weight: 600;
+        color: goldenrod;
+    }
+    .code-badge-copy-icon {
+        font-size: 1.2em;
+        cursor: pointer;
+        padding: 0 7px;
+        margin-top:2;
+    }
+</style>
+<div id="CodeBadgeTemplate" style="display:none">
+    <div class="code-badge">
+        <div class="code-badge-language">{{language}}</div>
+        <div title="Copy to clipboard">
+            <i class="{{copyIconClass}} code-badge-copy-icon"></i>
+        </div>
+     </div>
+</div>
+*/
